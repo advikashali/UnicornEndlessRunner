d82863a88a8e79f8579ec552751a8a89015d579b
Advika Shali <advikashali@gmail.com>
2023-05-07 01:59:59 +0100

    Imported unicorn assets
    Almost finished 3.
    Got movement
    Got changing direction
    Animation
    Player motion and camera motion

---
+++ UnicornGame/Assets/MyScenes/Scripts/CameraMotion.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+
+    public Transform lookatTarget; //unicorn
+    public Vector3 offset = new Vector3(0f, -2f, -1f);
+
+    private void Start()
+    {
+        transform.position = lookatTarget.position + offset;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = lookatTarget.position + offset;
+        desiredPosition.x = 0f;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime);
+    }
+}
---
+++ UnicornGame/Assets/SubwaySkater-master/SubwaySkater-master/README.md
@@ -0,0 +1,3 @@
+# SubwaySkater
+Subway skater game using Unity 3D based on [N3K EN YouTube tutorial](https://www.youtube.com/playlist?list=PLLH3mUGkfFCXQcNBz_FZDpqJfQlupTznd)
+
